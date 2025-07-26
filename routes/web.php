613cc90fe234b17085c3bc9a8ca3d4384e6a8c61
bor-ges <caiobges@hotmail.com>
2025-07-25 23:14:44 -0300

    Telas certificado - V1

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // routes/web.php
+    Route::get('/registrar-horas', function () {
+        return view('registrar-horas');})->name('registrar.horas');
+
+    Route::get('/meus-certificados', function () {
+        return view('meus-certificados');})->name('meus.certificados');
 });
 
 require __DIR__.'/auth.php';
